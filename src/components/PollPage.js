f5d3fac96624523b593f3757a17fa5e00288a468
Thuan Bui <75189396+bcthuan12@users.noreply.github.com>
2024-10-20 18:57:51 +0700

    modify pollpage (without css)

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,5 +1,35 @@
-const PollPage = () => {
-    return <div></div>
-}
+import { connect } from "react-redux";
+import { Navigate, useParams } from "react-router-dom";
 
-export default PollPage;
+const PollPage = ({ loggedIn, question, userId }) => {
+  const redirectUrl = window.location.href
+    .toString()
+    .split(window.location.host)[1];
+
+  return loggedIn ? (
+    <div>
+      <h1>Poll by {question.author}</h1>
+      <h2>Would you rather?</h2>
+      <div>
+        <h6>{question?.optionOne?.text}</h6>
+        <button>Click</button>
+      </div>
+      <div>
+        <h6>{question?.optionTwo?.text}</h6>
+        <button>Click</button>
+      </div>
+    </div>
+  ) : (
+    <Navigate to={`/login?redirectTo=${redirectUrl}`} />
+  );
+};
+
+const mapStateToProps = ({ authorizedUser, questions }) => {
+  return {
+    loggedIn: !!authorizedUser,
+    question: Object.values(questions).find((q) => q.id === useParams().id),
+    userId: authorizedUser?.id,
+  };
+};
+
+export default connect(mapStateToProps)(PollPage);
