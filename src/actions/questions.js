5959b892f55d6be72211d929c574d4e23ca734fe
Thuan Bui <75189396+bcthuan12@users.noreply.github.com>
2024-10-20 20:30:09 +0700

    add question (without css)

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,26 @@
+import { saveQuestion } from "../utils/apis";
+
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const ADD_QUESTION = "ADD_QUESTION";
 
 export const receiveQuestions = (questions) => {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
+  };
+};
+
+export const addQuestion = (question) => {
+  return {
+    type: ADD_QUESTION,
+    question,
+  };
+};
+
+export const createNewQuestion = (question) => {
+  return (dispatch) => {
+    return saveQuestion(question).then((q) => {
+      dispatch(addQuestion(q));
+    });
   };
 };
--- src/components/PollCreationPage.js
+++ src/components/PollCreationPage.js
@@ -1,5 +1,68 @@
-const PollCreationPage = () => {
-    return <div></div>
-}
+import { useState } from "react";
+import { connect } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import { createNewQuestion } from "../actions/questions";
 
-export default PollCreationPage;
+const PollCreationPage = ({ loggedIn, dispatch, userId }) => {
+  const redirectUrl = window.location.href
+    .toString()
+    .split(window.location.host)[1];
+
+  const [firstOption, setFirstOption] = useState("");
+  const [secondOption, setSecondOption] = useState("");
+
+  const inputFirstOption = (e) => setFirstOption(e.target.value);
+  const inputSecondOption = (e) => setSecondOption(e.target.value);
+
+  const navigate = useNavigate();
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(
+      createNewQuestion({
+        optionOneText: firstOption,
+        optionTwoText: secondOption,
+        author: userId,
+      })
+    );
+    navigate("/");
+  };
+
+  return loggedIn ? (
+    <div>
+      <h2>Would you rather</h2>
+      <h4>Create Your Own Poll</h4>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="firstOption">First Option</label>
+          <input
+            value={firstOption}
+            onChange={inputFirstOption}
+            name="firstOption"
+            id="firstOption"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="secondOption">Second Option</label>
+          <input
+            value={secondOption}
+            onChange={inputSecondOption}
+            name="secondOption"
+            id="firstOption"
+          ></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  ) : (
+    <Navigate to={`/login?redirectTo=${redirectUrl}`} />
+  );
+};
+
+const mapStateToProps = ({ authorizedUser }) => {
+  return {
+    loggedIn: !!authorizedUser,
+    userId: authorizedUser?.id,
+  };
+};
+
+export default connect(mapStateToProps)(PollCreationPage);
